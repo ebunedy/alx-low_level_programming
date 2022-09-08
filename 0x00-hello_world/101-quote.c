26c5dac38b451651a4a6fa2ce508917c0560d433
= <ebunedy@gmail.com>
2022-09-08 22:04:00 +0000

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /* main function*/
 int main(void)
 {
-	/*a C programme that uses puts function to print an output*/
-	char mystring[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	/*puts function*/
-	puts(mystring);
-	/*main() function return value*/
-	return (0);
+	/*a C programme that print below output output*/
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
