3063a93179c5a77687c1217f915a5477f60ac1c6
= <ebunedy@gmail.com>
2022-09-08 18:24:33 +0000

    betty fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+
+/*main function*/
+
 int main(void)
 {
 	/*a C programme that uses puts function to print an output*/
+
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(Mystr);
+
 	return (0);
 }
