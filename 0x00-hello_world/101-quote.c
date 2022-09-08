490898da212d963d4efe3b2082d9bebcea0a9428
= <ebunedy@gmail.com>
2022-09-08 21:55:02 +0000

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* main function*/
+int main(void)
+{
+	/*a C programme that uses puts function to print an output*/
+	char mystring[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	/*puts function*/
+	puts(mystring);
+	/*main() function return value*/
+	return (0);
+}
