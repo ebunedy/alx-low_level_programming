d21b79257bad15acea5741946f9e7e83085eb193
= <ebunedy@gmail.com>
2022-09-08 19:04:31 +0000

    a C program that prints exactly with proper grammar, but the outcome is a piece of art

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*main function*/
-
+/* main function*/
 int main(void)
 {
 	/*a C programme that uses puts function to print an output*/
-
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-
+	/*puts function*/
 	puts(Mystr);
-
+	/*main() function return value*/
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* main function*/
+int main(void)
+{
+	
+	/*printf function*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/*main() function return value*/
+	return (0);
+}
