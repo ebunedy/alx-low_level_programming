77d3d047545dc12b0c79d17496c59d4c40b9ccb5
= <ebunedy@gmail.com>
2022-09-09 15:25:06 +0000

    a program that prints all single digit numbers of base 10 starting from

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - prints digit numbers of base 10 starting from 0
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
+
