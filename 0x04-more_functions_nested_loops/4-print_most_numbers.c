877bd945796697e8ba864e26a821fa1750c451ac
= <ebunedy@gmail.com>
2022-09-15 18:51:36 +0000

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_most_numbers - print 0-9 with the exception of 2 and 4
+ * Return: 0 on success
+*/
+
+void print_most_numbers(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		if (i != '2' || i != '4')
+		{
+			_putchar(i);
+		}
+		++i;
+	}
+	_putchar('\n');
+}
