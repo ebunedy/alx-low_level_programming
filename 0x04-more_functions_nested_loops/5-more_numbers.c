0027bf5d6eff0e5a80c9850e512b033463e803e3
= <ebunedy@gmail.com>
2022-09-15 19:16:56 +0000

    task5 fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,20 @@
 */
 void more_numbers(void)
 {
-	char i = '0';
-	char j = '0';
+	char i = 0;
+	char j = 0;
 
-	while (i < '10')
+	while (i < 10)
 	{
-		while (j <= '14')
+		while (j <= 14)
 		{
-			_putchar(i);
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 			++j;
 		}
 		_putchar('\n');
+		j = 0;
 		++i;
 	}
-	_putchar('\n');
 }
