d348f6c154ebf6064742c0a1f36c2047f0a94b81
= <ebunedy@gmail.com>
2022-09-15 17:21:58 +0000

    task2 added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *mul - multiply two numbers
+ * @a: fisrt number
+ * @b: second number
+ * Return: result
+*/
+
+int mul(int a, int b);
+{
+
+	return (a * b);
+}
