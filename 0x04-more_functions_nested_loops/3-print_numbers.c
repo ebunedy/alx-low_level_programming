77381cdc4242d92fa5a7e5d55de0ef3d4b51a856
= <ebunedy@gmail.com>
2022-09-15 18:22:19 +0000

    nawao

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  *print_numbers - print 0-9
- * Return: 0-9
+ * Return: 0 on success
 */
-
 void print_numbers(void)
 {
 	char i = '0';
@@ -15,6 +13,5 @@ void print_numbers(void)
 		_putchar(i);
 		++i;
 	}
-
 	_putchar('\n');
 }
