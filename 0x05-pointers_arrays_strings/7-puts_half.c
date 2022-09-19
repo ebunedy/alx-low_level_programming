38d52a45956861a411552e6815c6414619a2769c
= <ebunedy@gmail.com>
2022-09-19 19:04:01 +0000

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - print half a string. followed by a new line
+ * @str: input to print
+ **/
+
+void puts_half(char *str)
+{
+	int j, i;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	if (i % 2 == 0)
+		i = j / 2;
+	else
+		i = (j + 1) / 2;
+	while (i < i)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
