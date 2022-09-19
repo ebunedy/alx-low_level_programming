b189b834be30c5b8df76eaba76889d8acc8b1e69
= <ebunedy@gmail.com>
2022-09-19 17:13:08 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: input to print
+ **/
+
+void print_rev(char *s)
+{
+	int j;
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	j--;
+	while (s[j] != '\0')
+	{
+		_putchar(s[j--]);
+	}
+	_putchar('\n');
+}
