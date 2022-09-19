7bb1c43e337b025c5c866d35b953f6d3a4e0465a
= <ebunedy@gmail.com>
2022-09-19 16:00:45 +0000

    task 0 and task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - update value of pointer
+ * @n: value to reset
+ **/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * swap_int - switches value of first input with the value of the second input.
+ * @a: First input to swap.
+ * @b: Second input to swap.
+ **/
+
+void swap_int(int *a, int *b)
+{
+	int tmpvalue;
+
+	*a = *b;
+	*b = tmpvalue;
+}
